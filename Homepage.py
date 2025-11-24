4f99edd2997a04ae57e81f3353aa4ed79a91c60f
JunF16 <junfontanos16@gmail.com>
2025-11-24 19:51:37 +0800

    added to Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -81,7 +81,7 @@ pg = st.navigation(
 )
 
 # Load and display the audio player
-audio_file = "audio/Nuvolebianche.ogg"
+audio_file = "audio/pixabay music.mp3"
 st.audio(audio_file, loop=True, autoplay=False) # autoplay=False is better for user experience
 
 # Combined Date and Time Display (Client-side)
